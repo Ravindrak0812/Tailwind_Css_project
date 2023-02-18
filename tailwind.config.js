7dd205a94eabb856cacfecf4c9004f32655a4c33
Ravindra Kumar <107619088+Ravindrak0812@users.noreply.github.com>
2023-02-18 22:55:22 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      spacing:{
+        13:'3.25rem'
+      }
+    },
+  },
+  plugins: [],
+}
